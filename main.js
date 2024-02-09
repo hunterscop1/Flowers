f75989e8c41b5995691bc6440ca418e894f95a2d
hunterscop1 <89689730+hunterscop1@users.noreply.github.com>
2024-02-09 14:17:32 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 onload = () =>{
         document.body.classList.remove("container");
-};
-
-function onAutoplay() {
-var x = document.getElementById("musik")
-x.autoplay.loop = true;
-x.load();
+        var x = document.getElementById("musik");
+        x.autoplay.loop = true;
+        x.load();
 }
